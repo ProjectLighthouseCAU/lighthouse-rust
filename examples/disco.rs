e9a1efad82eb98c86f39ee5559487b6c2ec1bbad
fwcd <fwcdmail@gmail.com>
2022-02-12 14:01:15 +0100

    Add logging

--- examples/disco.rs
+++ examples/disco.rs
@@ -1,5 +1,6 @@
 use async_std::task;
 use lighthouse_client::{Color, Connection, Authentication, LighthouseResult, Display};
+use log::{info, Level};
 use rand::prelude::*;
 use std::{env, time::Duration};
 
@@ -9,17 +10,23 @@ fn random_color() -> Color {
 }
 
 async fn run(auth: Authentication) -> LighthouseResult<()> {
+    info!("Connecting to Lighthouse server...");
     let mut conn = Connection::new(auth).await?;
-    
+
+    info!("Connected!");
     loop {
         conn.send_display(Display::fill(random_color())).await?;
+        info!("Sent display");
         task::sleep(Duration::from_secs(1)).await;
     }
 }
 
 fn main() {
+    simple_logger::init_with_level(Level::Info).unwrap();
+
     let username = env::var("LIGHTHOUSE_USERNAME").unwrap();
     let token = env::var("LIGHTHOUSE_TOKEN").unwrap();
     let auth = Authentication::new(username.as_str(), token.as_str());
+
     task::block_on(run(auth)).unwrap();
 }
