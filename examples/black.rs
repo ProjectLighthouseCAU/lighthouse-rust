5f02d2c946b451096a2e80695c37c99583aff09c
fwcd <fwcd@fwcd.dev>
2024-03-15 02:13:06 +0100

    Add example for sending a black frame

---
+++ examples/black.rs
@@ -0,0 +1,27 @@
+use lighthouse_client::{Lighthouse, Authentication, Result, Frame, Color};
+use tracing::info;
+use tokio::time;
+use std::{env, time::Duration};
+
+async fn run(auth: Authentication) -> Result<()> {
+    let mut lh = Lighthouse::connect_with_tokio(auth).await?;
+    info!("Connected to the Lighthouse server");
+
+    loop {
+        lh.put_model(Frame::fill(Color::BLACK)).await?;
+        info!("Sent frame");
+
+        time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let username = env::var("LIGHTHOUSE_USER").unwrap();
+    let token = env::var("LIGHTHOUSE_TOKEN").unwrap();
+    let auth = Authentication::new(username.as_str(), token.as_str());
+
+    run(auth).await.unwrap();
+}
