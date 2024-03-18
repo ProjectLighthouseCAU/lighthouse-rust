861535376015d2ff40acf3fe50cef9c28e3b6920
fwcd <fwcd@fwcd.dev>
2024-03-18 03:51:24 +0100

    Add example for getting metrics

---
+++ lighthouse-client/examples/admin_get_metrics.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use lighthouse_client::{protocol::Authentication, Lighthouse, Result, TokioWebSocket, LIGHTHOUSE_URL};
+use lighthouse_protocol::Value;
+use tracing::info;
+
+async fn run(mut lh: Lighthouse<TokioWebSocket>) -> Result<()> {
+    info!("Connected to the Lighthouse server");
+
+    let metrics: Value = lh.get(&["metrics", "laser"]).await?.payload;
+    info!("Got {}", metrics);
+
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Args {
+    /// The username.
+    #[arg(short, long, env = "LIGHTHOUSE_USER")]
+    username: String,
+    /// The API token.
+    #[arg(short, long, env = "LIGHTHOUSE_TOKEN")]
+    token: String,
+    /// The server URL.
+    #[arg(long, env = "LIGHTHOUSE_URL", default_value = LIGHTHOUSE_URL)]
+    url: String,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt().init();
+    _ = dotenvy::dotenv();
+
+    let args = Args::parse();
+    let auth = Authentication::new(&args.username, &args.token);
+    let lh = Lighthouse::connect_with_tokio_to(&args.url, auth).await?;
+
+    run(lh).await
+}
