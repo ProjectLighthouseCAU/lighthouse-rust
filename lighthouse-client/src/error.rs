4b8bf33e90a3162aaa2e9b5114a98aa83ca1c633
fwcd <fwcd@fwcd.dev>
2024-03-18 21:01:45 +0100

    Use thiserror to implement error enum

--- lighthouse-client/src/error.rs
+++ lighthouse-client/src/error.rs
@@ -1,36 +1,27 @@
 use async_tungstenite::tungstenite;
 use lighthouse_protocol::ValueError;
+use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// The type for any error involved in communication with the lighthouse.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum Error {
-    Tungstenite(tungstenite::Error),
-    Encode(rmp_serde::encode::Error),
-    Decode(rmp_serde::decode::Error),
-    Value(ValueError),
+    #[error("Tungstenite (WebSocket) error: {0}")]
+    Tungstenite(#[from] tungstenite::Error),
+    #[error("MessagePack encoding error: {0}")]
+    Encode(#[from] rmp_serde::encode::Error),
+    #[error("MessagePack decoding error: {0}")]
+    Decode(#[from] rmp_serde::decode::Error),
+    #[error("MessagePack value error: {0}")]
+    Value(#[from] ValueError),
+    #[error("Server error: {} {} (warnings: {:?})", code, message.clone().unwrap_or_else(|| "(no message)".to_string()), warnings)]
     Server { code: i32, message: Option<String>, warnings: Vec<String> },
+    #[error("Custom error")]
     Custom(String),
 }
 
 impl Error {
     /// Creates a new `LighthouseError` from the given custom message.
     pub fn custom(s: &str) -> Self { Self::Custom(s.to_owned()) }
-}
-
-impl From<tungstenite::Error> for Error {
-    fn from(e: tungstenite::Error) -> Self { Self::Tungstenite(e) }
-}
-
-impl From<rmp_serde::encode::Error> for Error {
-    fn from(e: rmp_serde::encode::Error) -> Self { Self::Encode(e) }
-}
-
-impl From<rmp_serde::decode::Error> for Error {
-    fn from(e: rmp_serde::decode::Error) -> Self { Self::Decode(e) }
-}
-
-impl From<ValueError> for Error {
-    fn from(e: ValueError) -> Self { Self::Value(e) }
 }
