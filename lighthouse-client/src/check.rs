2afe487b9b5640ca1041c9ba6c83f23b1d04e2a3
fwcd <30873659+fwcd@users.noreply.github.com>
2024-03-19 04:59:50 +0100

    Accept all 2xy codes

--- lighthouse-client/src/check.rs
+++ lighthouse-client/src/check.rs
@@ -8,7 +8,7 @@ pub trait Check: Sized {
 
 impl<P> Check for ServerMessage<P> {
     fn check(self) -> Result<Self> {
-        if self.code == 200 {
+        if self.code >= 200 && self.code < 300 {
             Ok(self)
         } else {
             Err(Error::Server { code: self.code, message: self.response, warnings: self.warnings })
