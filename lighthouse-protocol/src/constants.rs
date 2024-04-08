38c2f55f4e90dcc7e54ffce41821ab9a90e2f7ae
fwcd <fwcd@fwcd.dev>
2024-04-08 02:33:59 +0200

    Add `LIGHTHOUSE_BYTES`

--- lighthouse-protocol/src/constants.rs
+++ lighthouse-protocol/src/constants.rs
@@ -4,7 +4,9 @@ use crate::{Rect, Pos, Delta};
 pub const LIGHTHOUSE_ROWS: usize = 14;
 /// The number of columns of the lighthouse.
 pub const LIGHTHOUSE_COLS: usize = 28;
-/// The lighthouse's size.
+/// The number of pixels in a lighthouse frame.
 pub const LIGHTHOUSE_SIZE: usize = LIGHTHOUSE_ROWS * LIGHTHOUSE_COLS;
+/// The total number of bytes in a lighthouse frame.
+pub const LIGHTHOUSE_BYTES: usize = LIGHTHOUSE_SIZE * 3;
 /// The rectangle of valid coordinates on the lighthouse.
 pub const LIGHTHOUSE_RECT: Rect = Rect::new(Pos::ZERO, Delta::new(LIGHTHOUSE_COLS as i32, LIGHTHOUSE_ROWS as i32));
