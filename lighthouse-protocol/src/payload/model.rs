e2167f4a913cd213979f7e8ce89fe6562e4b538c
fwcd <fwcd@fwcd.dev>
2025-03-04 04:40:08 +0100

    Rename InputEvent to LegacyInputEvent

--- lighthouse-protocol/src/payload/model.rs
+++ lighthouse-protocol/src/payload/model.rs
@@ -1,11 +1,11 @@
 use serde::{Serialize, Deserialize};
 
-use crate::{Frame, InputEvent};
+use crate::{Frame, LegacyInputEvent};
 
 /// The payload of a model message.
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
 #[serde(untagged)]
 pub enum Model {
     Frame(Frame),
-    InputEvent(InputEvent),
+    InputEvent(LegacyInputEvent),
 }
