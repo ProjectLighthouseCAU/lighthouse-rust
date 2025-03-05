ceba5817f7e59d403898822719df172ea62fab6d
fwcd <fwcd@fwcd.dev>
2025-03-06 00:12:52 +0100

    Add GamepadButtonEvent::d_pad_direction

--- lighthouse-protocol/src/input/gamepad_button_event.rs
+++ lighthouse-protocol/src/input/gamepad_button_event.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::{Delta, Unity, Zero};
+
 /// A button event on a gamepad.
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 #[serde(tag = "control", rename_all = "camelCase")]
@@ -10,4 +12,18 @@ pub struct GamepadButtonEvent {
     pub down: bool,
     /// The value of the button (between 0.0 and 1.0, modeled after the Web Gamepad API).
     pub value: f64,
+}
+
+impl GamepadButtonEvent {
+    /// The direction if one of the D-pad buttons was pressed.
+    /// See https://www.w3.org/TR/gamepad/#dfn-standard-gamepad
+    pub fn d_pad_direction<T>(&self) -> Option<Delta<T>> where T: Zero + Unity {
+        match self.index {
+            12 => Some(Delta::UP),
+            13 => Some(Delta::DOWN),
+            14 => Some(Delta::LEFT),
+            15 => Some(Delta::RIGHT),
+            _ => None,
+        }
+    }
 }
