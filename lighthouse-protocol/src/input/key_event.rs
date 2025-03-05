a9b427fd04cc450739814f56646c779548d82a1d
fwcd <fwcd@fwcd.dev>
2025-03-06 00:05:38 +0100

    Add helpers for extracting direction from KeyEvent

--- lighthouse-protocol/src/input/key_event.rs
+++ lighthouse-protocol/src/input/key_event.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::{Delta, Unity, Zero};
+
 use super::{EventSource, KeyModifiers};
 
 /// A keyboard event.
@@ -16,4 +18,33 @@ pub struct KeyEvent {
     pub code: String, // TODO: Extract stronger `Key` type
     /// The held key modifiers.
     pub modifiers: KeyModifiers,
+}
+
+impl KeyEvent {
+    /// The direction if either the WASD or arrow keys were pressed.
+    pub fn direction<T>(&self) -> Option<Delta<T>> where T: Zero + Unity {
+        self.wasd_direction().or_else(|| self.arrow_direction())
+    }
+
+    /// The direction if one of the WASD keys was pressed.
+    pub fn wasd_direction<T>(&self) -> Option<Delta<T>> where T: Zero + Unity {
+        match self.code.as_str() {
+            "KeyW" => Some(Delta::UP),
+            "KeyA" => Some(Delta::LEFT),
+            "KeyS" => Some(Delta::DOWN),
+            "KeyD" => Some(Delta::RIGHT),
+            _ => None,
+        }
+    }
+
+    /// The direction if one of the arrow keys was pressed.
+    pub fn arrow_direction<T>(&self) -> Option<Delta<T>> where T: Zero + Unity {
+        match self.code.as_str() {
+            "ArrowUp" => Some(Delta::UP),
+            "ArrowLeft" => Some(Delta::LEFT),
+            "ArrowDown" => Some(Delta::DOWN),
+            "ArrowRight" => Some(Delta::RIGHT),
+            _ => None,
+        }
+    }
 }
