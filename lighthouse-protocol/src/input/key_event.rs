3d444e46577395f0feb4e4e9b514167741f1b7a6
fwcd <fwcd@fwcd.dev>
2025-03-05 04:22:18 +0100

    Update key API to new code and modifier fields

--- lighthouse-protocol/src/input/key_event.rs
+++ lighthouse-protocol/src/input/key_event.rs
@@ -10,6 +10,16 @@ pub struct KeyEvent {
     pub source: EventSource,
     /// Whether the key was pressed.
     pub down: bool,
-    /// The key pressed, see the docs on JS's `KeyboardEvent.key` for details.
-    pub key: String, // TODO: Extract stronger `Key` type
+    /// Whether the event is a repeat event.
+    pub repeat: bool,
+    /// The key pressed, see the docs on JS's `KeyboardEvent.code` for details.
+    pub code: String, // TODO: Extract stronger `Key` type
+    /// Whether the alt key is held.
+    pub alt_key: bool,
+    /// Whether the ctrl key is held.
+    pub ctrl_key: bool,
+    /// Whether the meta key is held.
+    pub meta_key: bool,
+    /// Whether the shiftKey key is held.
+    pub shift_key: bool,
 }
