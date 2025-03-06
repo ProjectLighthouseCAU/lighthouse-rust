54311952299c17d83c3f7c905a2928f228876dc3
fwcd <fwcd@fwcd.dev>
2025-03-06 18:44:25 +0100

    Factor out direction methods on GamepadEvent

--- lighthouse-protocol/src/input/gamepad_event.rs
+++ lighthouse-protocol/src/input/gamepad_event.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::Direction;
+
 use super::{EventSource, GamepadControlEvent};
 
 /// A gamepad/controller event.
@@ -11,4 +13,30 @@ pub struct GamepadEvent {
     /// The control-specific info.
     #[serde(flatten)]
     pub control: GamepadControlEvent,
+}
+
+impl GamepadEvent {
+    /// Parses the gamepad event as an arbitrary direction.
+    pub fn direction(&self) -> Option<Direction> {
+        self.left_direction().or_else(|| self.right_direction())
+    }
+
+    /// The direction if the gamepad event represents a D-pad or left stick.
+    /// Commonly used e.g. for movement in games.
+    pub fn left_direction(&self) -> Option<Direction> {
+        match &self.control {
+            GamepadControlEvent::Button(button) => button.d_pad_direction(),
+            GamepadControlEvent::Axis2D(axis2d) if axis2d.index == 0 => axis2d.direction(),
+            _ => None,
+        }
+    }
+
+    /// The direction if the gamepad event represents a right stick event.
+    /// Commonly used e.g. for camera control in games.
+    pub fn right_direction(&self) -> Option<Direction> {
+        match &self.control {
+            GamepadControlEvent::Axis2D(axis2d) if axis2d.index == 1 => axis2d.direction(),
+            _ => None,
+        }
+    }
 }
