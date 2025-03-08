72bfc6b17ed05a9d5cb0a8030d6e33b5f2246c1f
fwcd <fwcd@fwcd.dev>
2025-03-08 02:09:22 +0100

    Add EventSource: Hash

--- lighthouse-protocol/src/input/event_source.rs
+++ lighthouse-protocol/src/input/event_source.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 /// An identifier that is unique per client + device combo.
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Hash)]
 #[serde(untagged)]
 pub enum EventSource {
     String(String),
